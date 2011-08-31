6b7eefd0e387a1d4d1c8f96a682a7fa25d0d9b44
gcorradini <gregcorradini@gmail.com>
2011-08-31 11:20:14 -0700

    added example 1 to examples

---
+++ examples/foss4g2011_example1/foss4g2011_example1.py
@@ -0,0 +1,64 @@
+"""
+/***************************************************************************
+ foss4g2011_example1
+                                 A QGIS plugin
+ Example #1 for FOSS4G 2011 Workshop
+                              -------------------
+        begin                : 2011-08-31
+        copyright            : (C) 2011 by FOSS4G
+        email                : info@cugos.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+# Import the PyQt and QGIS libraries
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+# Initialize Qt resources from file resources.py
+import resources
+# Import the code for the dialog
+from foss4g2011_example1dialog import foss4g2011_example1Dialog
+
+class foss4g2011_example1:
+
+    def __init__(self, iface):
+        # Save reference to the QGIS interface
+        self.iface = iface
+
+    def initGui(self):
+        # Create action that will start plugin configuration
+        self.action = QAction(QIcon(":/plugins/foss4g2011_example1/icon.png"), \
+            "Example #1 for FOSS4G 2011 Workshop", self.iface.mainWindow())
+        # connect the action to the run method
+        QObject.connect(self.action, SIGNAL("triggered()"), self.run)
+
+        # Add toolbar button and menu item
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu("&Example #1 for FOSS4G 2011 Workshop", self.action)
+
+    def unload(self):
+        # Remove the plugin menu item and icon
+        self.iface.removePluginMenu("&Example #1 for FOSS4G 2011 Workshop",self.action)
+        self.iface.removeToolBarIcon(self.action)
+
+    # run method that performs all the real work
+    def run(self):
+
+        # create and show the dialog
+        dlg = foss4g2011_example1Dialog()
+        # show the dialog
+        dlg.show()
+        result = dlg.exec_()
+        # See if OK was pressed
+        if result == 1:
+            # do something useful (delete the line containing pass and
+            # substitute with your code
+            pass
---
+++ examples/foss4g2011_example1/foss4g2011_example1dialog.py
@@ -0,0 +1,30 @@
+"""
+/***************************************************************************
+ foss4g2011_example1Dialog
+                                 A QGIS plugin
+ Example #1 for FOSS4G 2011 Workshop
+                             -------------------
+        begin                : 2011-08-31
+        copyright            : (C) 2011 by FOSS4G
+        email                : info@cugos.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+from PyQt4 import QtCore, QtGui
+from ui_foss4g2011_example1 import Ui_foss4g2011_example1
+# create the dialog for zoom to point
+class foss4g2011_example1Dialog(QtGui.QDialog):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        # Set up the user interface from Designer.
+        self.ui = Ui_foss4g2011_example1()
+        self.ui.setupUi(self)
