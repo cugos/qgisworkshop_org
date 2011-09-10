c339970ca846ad21f65debf54b2fe3177c6a41de
gcorradini <gregcorradini@gmail.com>
2011-09-09 21:25:51 -0700

    finalized example 4 for 2nd hour material

---
+++ docs/source/_static/featureselect_1.py
@@ -0,0 +1,89 @@
+# Import the PyQt and QGIS libraries
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+from qgis.gui import *
+# Initialize Qt resources from file resources.py
+import resources
+# Import the code for the dialog
+from vector_selectbypointdialog import vector_selectbypointDialog
+
+class vector_selectbypoint:
+
+    def __init__(self, iface):
+        # Save reference to the QGIS interface
+        self.iface = iface
+        # refernce to map canvas
+        self.canvas = self.iface.mapCanvas()
+        # out click tool will emit a QgsPoint on every click
+        self.clickTool = QgsMapToolEmitPoint(self.canvas)
+        # create our GUI dialog
+        self.dlg = vector_selectbypointDialog()
+
+    def initGui(self):
+        # Create action that will start plugin configuration
+        self.action = QAction(QIcon(":/plugins/vector_selectbypoint/icon.png"), \
+            "some text that appears in the menu", self.iface.mainWindow())
+        # connect the action to the run method
+        QObject.connect(self.action, SIGNAL("triggered()"), self.run)
+
+        # Add toolbar button and menu item
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu("&some text that appears in the menu", self.action)
+
+        # connect our custom function to a clickTool signal that the canvas was clicked
+        result = QObject.connect(self.clickTool, SIGNAL("canvasClicked(const QgsPoint &, Qt::MouseButton)"), self.handleMouseDown)
+        #QMessageBox.information( self.iface.mainWindow(),"Info", "connect = %s"%str(result) )
+
+        # connect our select function to the canvasClicked signal
+        result = QObject.connect(self.clickTool, SIGNAL("canvasClicked(const QgsPoint &, Qt::MouseButton)"), self.selectFeature)
+        QMessageBox.information( self.iface.mainWindow(),"Info", "connect = %s"%str(result) )
+
+    def unload(self):
+        # Remove the plugin menu item and icon
+        self.iface.removePluginMenu("&some text that appears in the menu",self.action)
+        self.iface.removeToolBarIcon(self.action)
+
+    def handleMouseDown(self, point, button):
+        self.dlg.clearTextBrowser()
+        self.dlg.setTextBrowser( str(point.x()) + " , " +str(point.y()) )
+        #QMessageBox.information( self.iface.mainWindow(),"Info", "X,Y = %s,%s" % (str(point.x()),str(point.y())) )
+
+    def selectFeature(self, point, button):
+        QMessageBox.information( self.iface.mainWindow(),"Info", "in selectFeature function" )
+        # setup the provider select
+        pntGeom = QgsGeometry.fromPoint(point)
+        pntBuff = pntGeom.buffer(2.0,1) #buffer it 2 degrees and return with 1 segment
+        rect = pntGeom.boundingBox()
+        # get currentLayer and dataProvider
+        cLayer = self.canvas.currentLayer()
+        selectList = []
+        if cLayer:
+                provider = cLayer.dataProvider()
+                feat = QgsFeature()
+                # create the select statement
+                provider.select([],rect) # the arguments mean no attributes returned, and do a bbox filter with our buffered rectangle to limit the amount of features
+                while provider.nextFeature(feat):
+                    # if the feat geom returned from the selection intersects our point then put it in a list
+                    if feat.geometry().intersects(pntGeom):
+                        selectList.append(feat.id())
+
+                # make the actual selection
+                cLayer.setSelectedFeatures(selectList)
+        else:
+                QMessageBox.information( self.iface.mainWindow(),"Info", "No layer currently selected in TOC" )
+
+    # run method that performs all the real work
+    def run(self):
+        # make our clickTool the tool that we'll use for now
+        self.canvas.setMapTool(self.clickTool)
+
+        # show the dialog
+        self.dlg.show()
+        result = self.dlg.exec_()
+        # See if OK was pressed
+        if result == 1:
+            # do something useful (delete the line containing pass and
+            # substitute with your code
+            pass
+
