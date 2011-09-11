1993acb5814d38b14414c87d0d5aaace679d708e
thebigspoon <gregcorradini@gmail.com>
2011-09-11 13:08:09 -0700

    changed package script

--- examples/bashPackage.sh
+++ examples/bashPackage.sh
@@ -4,7 +4,9 @@ for FILE in $(find * -type d);
 do
     BEGTIMESTAMP_PDT=$(date)
     echo "\nSTART: " $FILE " at >>> " $BEGTIMESTAMP_PDT "\n" >> $logfile 2>&1
-    cd 
+    cd $FILE
+    rm resources.py
+    rm ui*.py
     make >> $logfile 2>&1
     cd ..
     zip -9vr $FILE.zip $FILE/* >> $logfile 2>&1
