0d384054b9b7aa21a786d79c761eb6e048a6b583
Aaron Racicot <aaronr@z-pulley.com>
2011-10-28 14:55:11 -0700

    Updates to add in google analytics

--- examples/bashPackage.sh
+++ examples/bashPackage.sh
@@ -10,6 +10,7 @@ do
     make >> $logfile 2>&1
     cd ..
     zip -9vr $FILE.zip $FILE/* >> $logfile 2>&1
+    mv *.zip ../www/plugins/dl/
     ENDTIMESTAMP_PDT=$(date)
     echo "\nEND: " $FILE " at >>> " $ENDTIMESTAMP_PDT "\n" >> $logfile 2>&1
 done;
